3c6e4be061459f8718cb2c402fea5d0e86777e25
570902003 <570902003@qq.com>
2018-07-06 12:12:37 +0800

    Initial commit

---
+++ src/sss.java
@@ -0,0 +1,16 @@
+public class sss {
+    private int a;
+
+    private String b;
+
+    public sss() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
